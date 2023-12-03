cc91e085b76c7794b3cb0c5654061479996ab904
Srirama Bhat <sriramarbhat@gmail.com>
2023-12-03 15:59:54 +0530

    Integrated file upload into Home component

--- src/components/Home/home.component.jsx
+++ src/components/Home/home.component.jsx
@@ -1,12 +1,18 @@
-import { useInView } from "react-intersection-observer";
-import FileUpload from "../file-upload/file-upload";
+// import { useInView } from "react-intersection-observer";
+import React, { useState } from "react";
+import { useDropzone } from "react-dropzone";
 import "./home.styles.scss";
 
 const Home = () => {
-  const [ref, inView] = useInView({
-    threshold: 0,
+
+  const [uploadedFiles, setUploadedFiles] = useState([]);
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop: (acceptedFiles) => {
+      setUploadedFiles(acceptedFiles);
+      // Call your backend API endpoint to upload files
+    },
   });
-  console.log(inView);
+
   return (
     <div class="home">
       <section className="analyzer-container">
@@ -23,7 +29,15 @@ const Home = () => {
         </div>
         <div className="analyzer">
           <div className="view">
-            <FileUpload />
+            <div {...getRootProps()}>
+              <input {...getInputProps()} />
+              <p>Drag and drop files here or click to browse.</p>
+              <ul>
+                {uploadedFiles.map((file) => (
+                  <li key={file.name}>{file.name}</li>
+                ))}
+              </ul>
+            </div>
           </div>
           <div className="result-container">
             <div className="result">
@@ -35,7 +49,7 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <section ref={ref} className="about-us"></section>
+      <section className="about-us"></section>
       <section className="methadology"></section>
     </div>
   );
