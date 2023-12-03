8cf6e51847bade2e2cbcd2416ac8a6ec500d8211
Srirama Bhat <sriramarbhat@gmail.com>
2023-12-03 11:03:27 +0530

    Added the signup and login functionalities to the forms

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,25 +1,77 @@
-// Login.js
-import React from 'react';
-import './Login.css'
-const Login = ({ label, ...otherProps }) => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Your login logic here
+import { useState } from "react";
+
+import {
+  signInAuthUserWithEmailAndPassword,
+} from "../../utils/firebase.utils";
+
+import FormInput from "../form-input/form-input";
+import "./login.styles.css"
+
+const defaultFormFields = {
+  email: "",
+  password: "",
+};
+
+const Login = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
+
+  // const { setCurrentUser } = useContext(UserContext);
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
   };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      await signInAuthUserWithEmailAndPassword(email, password);
+      // setCurrentUser(user);
+      resetFormFields();
+    } catch (error) {
+      if (
+        error.code === "auth/wrong-password" ||
+        error.code === "auth/user-not-found"
+      ) {
+        alert("Wrong Credentials");
+      } else {
+        console.log(error);
+      }
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
 
   return (
     <div className="login-container">
-      <form className="login-form" onSubmit={handleSubmit}>
-        <h2>Login</h2>
-        <div className="form-group">
-          <label htmlFor="username">Email:</label>
-          <input type="text" id="username" />
+      <h1>Login Form</h1>
+      <form className="login-form" action="" onSubmit={handleSubmit}>
+        <FormInput
+          label="Email"
+          type="email"
+          required
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
+
+        <FormInput
+          label="Password"
+          type="password"
+          required
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
+
+        <div className="buttons-container">
+          <button type="submit" className="form-btn">Login</button>
         </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" />
-        </div>
-        <button type="submit">Login</button>
       </form>
     </div>
   );
--- src/components/signup/signup.jsx
+++ src/components/signup/signup.jsx
@@ -1,28 +1,115 @@
-// SignUp.js
-import React from 'react';
-import './signup.scss'; // Import your SCSS file
-const SignUp = ({ label, ...otherProps }) => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Your sign-up logic here
+import { useState } from "react";
+
+import {
+  signInWithGooglePopup,
+  createAuthUserWithEmailAndPassword,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase.utils";
+// import { UserContext } from "../../context/user.context";
+import FormInput from "../form-input/form-input";
+import "./signup.styles.scss";
+
+const defaultFormFields = {
+  displayName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
+const SignUp = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { displayName, email, password, confirmPassword } = formFields;
+
+  // const { setCurrentUser } = useContext(UserContext);
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const logGoogleUser = async () => {
+    await signInWithGooglePopup();
+    // setCurrentUser(user);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert("Passwords do no match");
+      return;
+    }
+
+    try {
+      const { user } = await createAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+
+      // setCurrentUser(user);
+
+      await createUserDocumentFromAuth(user, { displayName });
+      resetFormFields();
+    } catch (error) {
+      if (error.code === "auth/email-already-in-use") {
+        alert("Cannot create user, email already in use");
+      } else {
+        console.log("User creation encountered an error", error);
+      }
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
   };
 
   return (
     <div className="signup-container">
-      <form className="signup-form" onSubmit={handleSubmit}>
-        <h2>Sign Up</h2>
-        <div className="form-group">
-          <label htmlFor="username">Username:</label>
-          <input type="text" id="username" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" />
-        </div>
-        <button type="submit">Sign Up</button>
-        <br />
-        <br />
-        <button type="submit">Google Login</button>
+      <h1>Sign Up Form</h1>
+      <form className="signup-form" action="" onSubmit={handleSubmit}>
+        <FormInput
+          label="Username"
+          type="text"
+          required
+          onChange={handleChange}
+          name="displayName"
+          value={displayName}
+        />
+
+        <FormInput
+          label="Email"
+          type="email"
+          required
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
+
+        <FormInput
+          label="Password"
+          type="password"
+          required
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
+
+        <FormInput
+          label="Confirm Password"
+          type="password"
+          id="uconpass"
+          required
+          onChange={handleChange}
+          name="confirmPassword"
+          value={confirmPassword}
+        />
+
+        <button type="submit" className="form-btn">
+          Sign Up
+        </button>
+        <button type="button" onClick={logGoogleUser} className="form-btn">
+            Login with Google
+        </button>
       </form>
     </div>
   );
